023ec5db71ae918b3d05706b751d46157b625808
xeureka <teklemariameureka@gmail.com>
2025-04-19 04:50:02 +0000

    registration is done

--- Vidly-express/routes/users.js
+++ Vidly-express/routes/users.js
@@ -3,12 +3,13 @@ const Users = require('../models/userSchema')
 const express = require('express')
 const mongoose = require('mongoose')
 const router = express.Router()
+const _ = require('lodash')
 // api end point = /api/users
 
 
 router.post('/',async (req,res) => {
 
-    let user = Users.findOne({email: req.body.email})
+    let user = await Users.findOne({email: req.body.email})
 
     if (user){
         return res.status(400).send('User Already registered !')
@@ -16,14 +17,12 @@ router.post('/',async (req,res) => {
 
     try {
 
-        const newUser = new Users({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-        })
+        user = new Users(_.pick(req.body, ['name','email','password']))
 
-        await newUser.save()
-        res.send(newUser)
+        await user.save()
+
+
+        res.send(_.pick(user,['_id','name','email']))
         
     } catch (error) {
         console.error('Error Registaering a user, ',error.message)

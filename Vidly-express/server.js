52e126ff7f67474328ce5c660b715fc0b5028e23
xeureka <teklemariameureka@gmail.com>
2025-04-21 07:31:43 +0000

    session and toeknization

--- Vidly-express/server.js
+++ Vidly-express/server.js
@@ -13,36 +13,3 @@
  * role based authorization
  * testing authorization
  */
-
-// how to cmpare and verify password wors in bcrypt
-
-// lets work on comparing auth thing
-
-
-const bcrypt = require('bcryptjs')
-
-// Fuction hashPassword
-
-
-async function hashPassword(password){
-    const salt = await bcrypt.genSalt(10)
-
-    const hashed = await bcrypt.hash(password,salt)
-
-    console.log(hashed)
-}
-
-// Function to verify password
-
-async function verifyPassword(password,hashedPassword){
-    try {
-        const match = await bcrypt.compare(password,hashedPassword)
-
-        console.log('Match: ',match)
-        
-    } catch (error) {
-        concole.error('Error verifying password: ',error.message)
-    }
-}
-
-verifyPassword('urka@2004','$2b$10$BEHNwnO9/0X3ho9.zRt0sez3P0UotPOVTH1F594oZCrHN8/hcdiDi')

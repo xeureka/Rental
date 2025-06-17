6229790c4220a308d2c2ecd5cf657fade8c10caf
xeureka <teklemariameureka@gmail.com>
2025-06-17 18:10:36 +1000

    remove: the mongoose models

--- ts-backend/src/index.ts
+++ ts-backend/src/index.ts
@@ -2,9 +2,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './utils/db.connect'
-import UsersRouter from './routes/users.router'
-import MovieRouter from './routes/movie.router'
-import GenreRouter from './routes/genre.router'
+
 
 dotenv.config()
 
@@ -12,9 +10,7 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 app.use(express.json())
-app.use('/api/users',UsersRouter);  // admin protected route
-app.use('/api/movies',MovieRouter)
-app.use('/api/genre',GenreRouter)
+
 
 app.get('/',(req,res) => {
     res.send('Hello World')
--- ts-backend/src/routes/reserve.router.ts
+++
@@ -1,2 +0,0 @@
-
-// Routes to reserve the seat on a prefered movie and date
